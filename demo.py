6a9024d8b35b22b491c6a2bdb5749d547d4deef3
Yang Liu <foamliu@yeah.net>
2018-07-19 08:09:39 +0800

    update model

--- demo.py
+++ demo.py
@@ -15,7 +15,7 @@ from model import build_model
 if __name__ == '__main__':
     channel = 3
 
-    model_weights_path = 'models/model.01-69.8118.hdf5'
+    model_weights_path = 'models/model.01-68.4077.hdf5'
     model = build_model()
     model.load_weights(model_weights_path)
 
